adfde279f9e2e27dbf0ad2e3b945f91b02446cce
majunhong <569847911@qq.com>
2018-12-06 00:44:44 +0800

    组合详情中的组合配置

---
+++ src/pages/compose-detail/compose-option.vue
@@ -0,0 +1,383 @@
+<!-- 组合配置 -->
+<template>
+  <div class="content">
+    <div class="chart-wrap">
+      <mpvue-echarts :echarts="echarts" :onInit="initChart" />
+    </div>
+    <div class="opts-detail">
+      <div class="detail-title">
+        <span class="txt">假设投资金额为: <span class="money">100000</span>元,配置如下:</span>
+        <div class="modify-money">修改金额</div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+      <div class="each-item">
+        <div class="line thead">
+          <span class="td">A股</span>
+          <span class="td">50000元</span>
+          <span class="td">50.0%</span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">国泰上证5年期国债ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+        <div class="line tr">
+          <span class="td">
+            <span class="name">华夏上证50ETF</span>
+            <span class="nums">510050.SH</span>
+          </span>
+          <span class="td">20000元</span>
+          <span class="td">20.0%</span>
+          <span class="arrow"></span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import * as echarts from 'echarts/lib/echarts';
+require('echarts/lib/chart/pie.js');
+require('echarts/lib/component/legend.js');
+import mpvueEcharts from 'mpvue-echarts';
+import { setTimeout } from 'timers';
+
+export default {
+  components: {
+    mpvueEcharts
+  },
+  props: {
+    id: {
+      type: Number,
+      default: 1,
+    }
+  },
+  data() {
+    return {
+      echarts,
+    }
+  },
+  methods: {
+    // 初始化表格
+    initChart(canvas, width, height) {
+      const vthis = this;
+      // 图形颜色
+      const pieColor = ['#345d9a', '#e65f43', '#6ba35b', '#7a61a4', '#399b9f', '#9f8c39'];
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      })
+      canvas.setChart(chart)
+      var option = {
+        legend: {
+          orient: 'vertical',
+          top: 'center',
+          left: '70%',
+          itemWidth: 15,
+          itemHeight: 15,
+          // data:['A股  50%','港股  5.0%','境外股票  5.0%','债券  30.0%','货币  10.0%']
+        },
+        series: [
+          {
+            type:'pie',
+            center: ['120', '50%'],
+            radius: ['58', '105'],
+            avoidLabelOverlap: false,
+            label: {
+              show: true,
+              position: 'inside',
+              formatter: '{d}%',
+            },
+          }
+        ]
+      }
+      // TODO 请求获取数据
+      setTimeout(() => {
+        console.log(vthis.id);
+        option.legend.data = ['A股  50%','港股  5.0%','境外股票  5.0%','债券  30.0%','货币  10.0%'];
+        option.series[0].data = [
+              {value:335, name:'A股  50%', itemStyle: {color: '#345d9a'}},
+              {value:310, name:'港股  5.0%', itemStyle: {color: pieColor[1]}},
+              {value:234, name:'境外股票  5.0%', itemStyle: {color: pieColor[2]}},
+              {value:135, name:'债券  30.0%', itemStyle: {color: pieColor[3]}},
+              {value:1548, name:'货币  10.0%', itemStyle: {color: pieColor[4]}}
+            ];
+        chart.setOption(option);
+        return chart;
+      }, 1000);
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+  .chart-wrap {
+    width: 100%;
+    height: 508rpx;
+    background-color: #fff;
+  }
+  .opts-detail {
+    .detail-title {
+      height: 80rpx;
+      padding: 0 20rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      .txt {
+        font-size: 28rpx;
+        color: #333333;
+        .money {
+          color: #e74612;
+        }
+      }
+      .modify-money {
+        width: 150rpx;
+        height: 50rpx;
+        line-height: 50rpx;
+        text-align: center;
+        font-size: 26rpx;
+        color: #fff;
+        background-color: #4768f3;
+        border-radius: 20rpx;
+      }
+    }
+    .each-item {
+      .line {
+        display: flex;
+        align-items: center;
+        .td {
+          flex-grow: 0;
+          flex-shrink: 0;
+          text-align: center;
+          &:nth-child(1) {
+            flex-basis: 340rpx;
+          }
+          &:nth-child(2) {
+            flex-basis: 185rpx;
+          }
+          &:nth-child(3) {
+            flex-basis: 165rpx;
+            padding-right: 60rpx;
+          }
+        }
+        &.thead {
+          .td {
+            font-size: 26rpx;
+            color: #fff;
+            height: 60rpx;
+            line-height: 60rpx;
+          }
+        }
+        &.tr {
+          position: relative;
+          .td {
+            font-size: 28rpx;
+            color: #333333;
+            background-color: #fff;
+            height: 100rpx;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
+          .arrow {
+            display: block;
+            width: 14rpx;
+            height: 25rpx;
+            background: url(../find/img/arrow.png) no-repeat 0 0;
+            background-size: cover;
+            position: absolute;
+            top: 34rpx;
+            right: 30rpx;
+          }
+        }
+      }
+      .tr + .tr {
+        border-top: 1rpx solid #e5e5e5;
+      }
+      &:nth-child(2) {
+        .thead {
+          background-color: #345d9a;
+        }
+      }
+      &:nth-child(3) {
+        .thead {
+          background-color: #e65f43;
+        }
+      }
+      &:nth-child(4) {
+        .thead {
+          background-color: #6ba35b;
+        }
+      }
+      &:nth-child(5) {
+        .thead {
+          background-color: #7a61a4;
+        }
+      }
+      &:nth-child(6) {
+        .thead {
+          background-color: #399b9f;
+        }
+      }
+      &:nth-child(7) {
+        .thead {
+          background-color: #9f8c39;
+        }
+      }
+    }
+  }
+</style>
--- src/pages/compose-detail/index.vue
+++ src/pages/compose-detail/index.vue
@@ -17,9 +17,224 @@
       </div>
       <div class="review-result">
         <div class="title">
-
+          <span class="txt">用户测评结果</span>
+          <div class="review-again">重新测评</div>
+        </div>
+        <div class="content">
+          <div class="item">
+            <span class="key">亏损承受度:</span>
+            <span class="value">9.0%</span>
+          </div>
+          <div class="item">
+            <span class="key">亏损厌恶度:</span>
+            <span class="value icon low">低</span>
+          </div>
+          <div class="item">
+            <span class="key">收益偏好:</span>
+            <span class="value icon middle">中</span>
+          </div>
+          <div class="item">
+            <span class="key">收益频率:</span>
+            <span class="value icon hight">高</span>
+          </div>
         </div>
       </div>
     </div>
+    <div class="main">
+      <div class="tab-area">
+        <div class="tab" :class="{'cur': tabIndex == 0}" @click="tabIndex = 0">组合配置</div>
+        <div class="tab" :class="{'cur': tabIndex == 1}" @click="tabIndex = 1">业绩表现</div>
+        <div class="tab" :class="{'cur': tabIndex == 2}" @click="tabIndex = 2">风险控制</div>
+        <div class="tab"  :class="{'cur': tabIndex == 3}" @click="tabIndex = 3">智能调仓</div>
+      </div>
+      <div class="each-content" v-show="tabIndex == 0">
+        <compose-option />
+      </div>
+      <div class="each-content" v-show="tabIndex == 1"></div>
+      <div class="each-content" v-show="tabIndex == 2"></div>
+      <div class="each-content" v-show="tabIndex == 3"></div>
+    </div>
+    <div class="bottom-float">
+      <navigator :url="'/pages/compose-detail/main'" :hover-class="'none'" class="start">开始投资</navigator>
+    </div>
   </div>
 </template>
+
+<script>
+
+import ComposeOption from './compose-option';
+
+export default {
+  components: {
+    ComposeOption,
+  },
+  data() {
+    return {
+      tabIndex: 0,
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.top-area {
+  width: 100%;
+  padding-top: 20rpx;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-bottom: 1rpx solid #e5e5e5;
+  margin-bottom: 20rpx;
+  .attr-box {
+    width: 690rpx;
+    height: 144rpx;
+    box-shadow: 0 0 40rpx #ebebeb;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 5rpx;
+    border-radius: 8rpx;
+    .item {
+      margin: 0 32rpx;
+      text-align: center;
+      .value {
+        font-size: 40rpx;
+        color: #e74612;
+        margin-bottom: 10rpx;
+      }
+      .key {
+        font-size: 26rpx;
+        color: #333333;
+      }
+    }
+  }
+  .review-result {
+    width: 690rpx;
+    .title {
+      height: 88rpx;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      border-bottom: 1rpx solid #e5e5e5;
+      .txt {
+        line-height: 28rpx;
+        border-left: 7rpx solid #4768f3;
+        padding-left: 13rpx;
+        font-size: 30rpx;
+        color: #333333;
+      }
+      .review-again {
+        width: 150rpx;
+        height: 50rpx;
+        line-height: 50rpx;
+        text-align: center;
+        font-size: 26rpx;
+        color: #ffffff;
+        background-color: #4768f3;
+        border-radius: 20rpx;
+      }
+    }
+    .content {
+      display: flex;
+      align-content: center;
+      flex-wrap: wrap;
+      height: 205rpx;
+      .item {
+        flex: 0 0 50%;
+        height: 68rpx;
+        line-height: 68rpx;
+        .key {
+          display: inline-block;
+          width: 200rpx;
+          text-align: right;
+          font-size: 28rpx;
+          color: #000000;
+        }
+        .value {
+          font-size: 28rpx;
+          color: #e74612;
+          margin-left: 10rpx;
+          &.icon {
+            display: inline-block;
+            width: 36rpx;
+            height: 36rpx;
+            line-height: 36rpx;
+            text-align: center;
+            font-size: 24rpx;
+            color: #fff;
+            border-radius: 50%;
+            &.low {
+              background-color: #4dba2d;
+            }
+            &.middle {
+              background-color: #ff9500;
+            }
+            &.hight {
+              background-color: #ff6a59;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+.main {
+  width: 100%;
+  margin-bottom: 80rpx;
+  .tab-area {
+    width: 100%;
+    height: 90rpx;
+    background-color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 1rpx solid #e5e5e5;
+    border-bottom: 1rpx solid #e5e5e5;
+    .tab {
+      font-size: 28rpx;
+      color: #999999;
+      padding: 0 30rpx;
+      position: relative;
+      &.cur {
+        color: #4768f3;
+      }
+      &.cur:after {
+        content: '';
+        display: block;
+        width: 50rpx;
+        height: 4rpx;
+        border-radius: 2rpx;
+        background-color: #95a7f2;
+        position: absolute;
+        top: 48rpx;
+        left: 50%;
+        margin-left: -25rpx;
+      }
+    }
+    .tab + .tab {
+      border-left: 1rpx solid #999;
+    }
+  }
+}
+.bottom-float {
+  width: 750rpx;
+  height: 80rpx;
+  background-color: #383b4a;
+  position: fixed;
+  bottom: 0;
+  .start {
+    width: 205rpx;
+    height: 46rpx;
+    line-height: 46rpx;
+    text-align: center;
+    font-size: 26rpx;
+    color: #ffffff;
+    background-color: #4768f3;
+    border-radius: 23rpx;
+    position: absolute;
+    top: 17rpx;
+    right: 30rpx;
+  }
+}
+</style>

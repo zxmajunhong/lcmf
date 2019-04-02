d66056fac0a49271b84c3c510fc2499038806a5e
majunhong <majunhong@tmeeyou.com>
2019-04-02 15:20:13 +0800

    1

--- src/pages/change-detail/index.vue
+++ src/pages/change-detail/index.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="container">
-      <div class="tab-content">
-        <div class="table-area">
-          <div class="table">
-            <div class="tr title">
-              <div class="td">证券代码</div>
-              <div class="td">证券名称</div>
-              <div class="td">持仓数量(调仓前)</div>
-              <div class="td">交易</div>
-              <div class="td">持仓数量(调仓后)</div>
-            </div>
-            <div class="tr" v-for ="info in buyList" :key="info.key">
-              <div class="td">{{info.code}}</div>
-              <div class="td">{{info.etfName}}</div>
-              <div class="td">{{info.before}}</div>
-              <div class="td">{{info.deal}}</div>
-              <div class="td">{{info.after}}</div>
-            </div>
+    <div class="table-area">
+      <div class="tr">
+        <div class="col1 first">
+        </div>
+        <div class="col2">
+          <div class="line title">
+            <div class="item"><span>证券代码</span></div>
+            <div class="item">证券名称</div>
+            <div class="item" style="line-height: inherit;">持仓数量(调仓前)</div>
+            <div class="item">交易</div>
+            <div class="item" style="line-height: inherit;">持仓数量(调仓后)</div>
           </div>
         </div>
       </div>
-  </div>  
+      <div class="tr" v-if="buyList && buyList.length > 0">
+        <div class="col1 sel">卖出</div>
+        <div class="col2">
+          <div class="line" v-for="item in buyList" :key="item.id">
+            <div class="item">{{item.code}}</div>
+            <div class="item">{{item.etfName}}</div>
+            <div class="item">{{item.before}}</div>
+            <div class="item">{{item.deal}}</div>
+            <div class="item">{{item.after}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="tr" v-if="selList && selList.length > 0">
+        <div class="col1 buy">买入</div>
+        <div class="col2">
+          <div class="line" v-for="item in sellList" :key="item.id">
+            <div class="item">{{item.code}}</div>
+            <div class="item">{{item.etfName}}</div>
+            <div class="item">{{item.before}}</div>
+            <div class="item">{{item.deal}}</div>
+            <div class="item">{{item.after}}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 import {getChangeDetail} from '@/utils/model';
@@ -45,17 +64,57 @@ export default {
 
 <style lang="less" scoped>
 .table-area {
+  width: 100%;
   background-color: #fff;
-  .table {
-    width: 100%;
-    font-size: 14rpx;
-    color: #424242;
-    .tr {
-      width: 100%;
-      display: flex;
-      .td {
-        flex: 0 0 33%;
-        text-align: center;
+  padding:0 10rpx;
+  font-size: 26rpx;
+  .tr {
+    display: flex;
+    align-items: stretch;
+    justify-content: center;
+    .col1 {
+      flex: 1 0 6%;
+      text-align: center;
+      font-size: 32rpx;
+      letter-spacing: 10rpx;
+      padding: 10rpx 0;
+      border: 1rpx solid #dfdfdf;
+
+      font-weight: bold;
+      &.sel {
+        background-color: rgb(150, 228, 119);
+        border-top: 0;
+      }
+      &.buy {
+        background-color: rgb(224, 75, 75);
+        border-top: 0;
+      }
+    }
+    .col2 {
+      flex: 1 0 94%;
+      .line {
+        display: flex;
+        align-items:center;
+        justify-content: center;
+        .item {
+          height: 50rpx;
+          line-height: 50rpx;
+          border: 1rpx solid #dfdfdf;
+          border-top: 0;
+          border-left: 0;
+          flex: 1 0 20%;
+          text-align: center;
+          vertical-align: middle;
+        }
+        &.title {
+         .item {
+           font-weight: bold;
+           font-size: 30rpx;
+           height: 80rpx;
+           line-height: 80rpx;
+           border-top: 1rpx solid #dfdfdf;
+         }
+        }
       }
     }
   }

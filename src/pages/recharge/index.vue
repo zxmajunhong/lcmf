7e2e76d68014592f9eebd585f02ee113795019f5
majunhong <majunhong@tmeeyou.com>
2019-04-03 18:43:04 +0800

    充值金币

--- src/pages/recharge/index.vue
+++ src/pages/recharge/index.vue
@@ -1,82 +1,89 @@
 <template>
 <div class="container">
-    <div class="invest-box">
+    <div class="recharge-box">
       <div class="total-line">
-        我的金币：{{money}}
+        我的金币：<span class="count">{{money}}</span>
       </div>
-      <div class="explain-line">
-        <div>
-          10金币
-          <div>售价：10.0元</div>
-        </div>  
-        <div>
-          30金币
-          <div>售价：30.0元</div>
-        </div> 
-        <div>
-          50金币
-          <div>售价：50.0元</div>
-        </div> 
-        <div>
-          100金币
-          <div>售价：100.0元</div>
-        </div> 
-        <div>
-          200金币
-          <div>售价：200.0元</div>
-        </div> 
-        <div>
-          500金币
-          <div>售价：500.0元</div>
-        </div> 
+      <div class="select-area">
+        <div class="item" v-for="(item, idx) in selectArry" :key="item" :class="{'cur': selectIndex == idx}" @click="itemSelect(idx)">
+          <div class="count">{{item.count}}金币</div>
+          <div class="price">售价：{{item.price}}元</div>
+        </div>
       </div>
       <div class="oper-line">
-        <div class="input-area">
-          <input type="text" class="invest-value" v-model="changeMoney">
-          <span class="unit">元</span>
+        <div class="customer">
+          <span>自定义：</span><input type="text" class="recharge-customer" placeholder="金币数量" @click="inputClick" @blur="getNums" v-model="inputVal">
         </div>
-        <div class="invest-submit" @click="checkRechage">确定</div>
+        <div class="recharge-submit" @click="checkRechage">确定</div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import {getUserMoney} from '@/utils/model';
+import {getUserMoney, getWxPayArgs} from '@/utils/model';
 export default {
 
   data() {
     return {
       money: 0,
+      selectArry: [{count: 10, price: 10.0}, {count: 30, price: 30.0}, {count: 50, price: 49.0}, {count: 100, price: 95.0}, {count: 200, price: 180.0}, {count: 500, price: 425.0}],
+      selectIndex: -1, // 选择第几个
+      rechargeNums: 0, // 兑换数量
+      inputVal: '', // 自定义数量
     }
   },
 
   methods: {
+    itemSelect(i) {
+      this.selectIndex = i;
+      this.rechargeNums = this.selectArry[i].count;
+    },
+    inputClick() {
+      this.selectIndex = -1;
+      this.rechargeNums = 0;
+    },
+    // 获取输入框的值
+    getNums() {
+      this.rechargeNums = this.inputVal;
+    },
     checkRechage(){
-        var money = this.changeMoney;
-        wx.showModal({showCancel:true, title:'确认充值', success(){
-              getWxPayArgs(money).then(res => {
-                if (res.code == 10000) {
-                  wx.requestPayment({
-                    timeStamp: res.data.timeStamp,
-                    nonceStr: res.data.nonceStr,
-                    package: res.data.package,
-                    signType: res.data.signType,
-                    paySign: res.data.paySign,
-                    'success': function (res) {
-                      that.afterPaySuccess(ret.data.orderID);
-                    },
-                    'fail': function (res) {
-                      console.log(res);
-                    }
-                  })  
-                } else {
-                  wx.showToast({
-                    title: res.msg,
-                    icon: 'none',
-                  })
-                }
-              })
-            }})
+      const val = this.rechargeNums;
+      if (!val) {
+        wx.showToast({
+          title: '请选择或者输入金币数量',
+          icon: 'none',
+        });
+        return false;
+      }
+      wx.showModal({
+        showCancel: true,
+        title:`确认充值${val}金币吗？`,
+        success(res){
+          if (res.confirm) {
+            getWxPayArgs(val).then(res => {
+              if (res.code == 10000) {
+                wx.requestPayment({
+                  timeStamp: res.data.timeStamp,
+                  nonceStr: res.data.nonceStr,
+                  package: res.data.package,
+                  signType: res.data.signType,
+                  paySign: res.data.paySign,
+                  'success': function (res) {
+                    that.afterPaySuccess(ret.data.orderID);
+                  },
+                  'fail': function (res) {
+                    console.log(res);
+                  }
+                })
+              } else {
+                wx.showToast({
+                  title: res.msg,
+                  icon: 'none',
+                })
+              }
+            })
+          };
+        }})
     }
   },
   onShow() {
@@ -89,95 +96,77 @@ export default {
 
 
 <style lang="less" scoped>
-  .invest-box {
-    color: #333333;
-    margin-bottom: 20rpx;
-    .total-line {
-      font-size: 32rpx;
-      margin-bottom: 20rpx;
-      .value {
-        color: #e74612;
-      }
+.recharge-box {
+  width: 100%;
+  background-color: #fff;
+  .total-line {
+    line-height: 50rpx;
+    border-bottom: 1rpx solid #dfdfdf;
+    font-size: 30rpx;
+    padding: 0 20rpx;
+    .count {
+      color: #e74612;
     }
-    .explain-line {
-      font-size: 28rpx;
-    }
-    .oper-line {
+  }
+  .select-area {
+    padding: 20rpx;
+    height: 250rpx;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    .item {
+      flex:0 0 30%;
+      height:100rpx;
+      border:1rpx solid #dfdfdf;
+      margin:10rpx 0;
+      text-align:center;
+      font-size:28rpx;
+      border-radius:8rpx;
       display: flex;
-      margin: 40rpx 0 40rpx 0;
-      .mpvue-picker {
-        width: 200rpx;
-        height: 50rpx;
-        border-radius: 8rpx;
-        border: 1rpx solid #999999;
-        margin-right: 10rpx;
-        line-height: 50rpx;
-        position: relative;
-        .value {
-          height: 50rpx;
-          line-height: 50rpx;
-          font-size: 30rpx;
-          color: #e74612;
-          padding-left: 30rpx;
-        }
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .price {
+        font-size: 26rpx;
       }
-      .mpvue-picker::after {
-        content: '';
-        display: block;
-        width: 0;
-        height: 0;
-        border-top: 10rpx solid #999;
-        border-left: 8rpx solid transparent;
-        border-right: 8rpx solid transparent;
-        position: absolute;
-        top: 20rpx;
-        right: 10rpx;
-      }
-      .input-area {
-        display: flex;
-        align-items: baseline;
-        font-size: 24rpx;
-        color: #333;
-        margin-right: 20rpx;
-        .invest-value {
-          width: 200rpx;
-          height: 50rpx;
-          padding-right: 20rpx;
-          border: 1rpx solid #999999;
-          border-radius: 8rpx;
-          text-align: right;
-          font-size: 28rpx;
-          color: #333;
-          margin-right: 10rpx;
-        }
-
-      }
-      .invest-submit {
-        width: 98rpx;
-        height: 50rpx;
-        line-height: 50rpx;
-        text-align: center;
-        border-radius: 25rpx;
-        font-size: 24rpx;
-        color: #fff;
-        background-color: #4768f3;
-        margin-top: 2rpx;
+      &.cur {
+        border-color: #4768f3;
       }
     }
   }
-  .record-redirect {
-    width: 100%;
-    padding-top: 50rpx;
+  .oper-line {
+    height: 80rpx;
     display: flex;
     justify-content: space-between;
-    font-size: 32rpx;
-    color: #333333;
-    background-color: #fff;
-    .title {
-      margin-left: 40rpx;
+    align-items: center;
+    padding: 0 20rpx;
+    .customer {
+      font-size: 28rpx;
+      display: flex;
+      align-items: center;
+      .recharge-customer {
+        width: 200rpx;
+        height: 50rpx;
+        padding: 0 10rpx;
+        border: 1rpx solid #999999;
+        border-radius: 8rpx;
+        font-size: 26rpx;
+        color: #333;
+        margin-left: 10rpx;
+      }
     }
-    .arrow {
-      margin-right: 40rpx;
+    .recharge-submit {
+      width: 98rpx;
+      height: 50rpx;
+      line-height: 50rpx;
+      text-align: center;
+      border-radius: 25rpx;
+      font-size: 24rpx;
+      color: #fff;
+      background-color: #4768f3;
+      margin-top: 2rpx;
     }
   }
+}
 </style>
